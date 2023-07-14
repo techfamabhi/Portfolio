6bafdc2fbdbd055e018db75f8ad182a3cdbf34f6
techfamabhi <68576675+techfamabhi@users.noreply.github.com>
2023-07-14 18:37:55 +0530

    react change code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,34 +7,40 @@ const Contact = () => {
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8">
+        <div className="pb-9">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
           </p>
-          <p className="py-6">Submit the form below to get in touch with me</p>
+          <p className="py-7">Submit the form below to get in touch with me</p>
         </div>
 
         <div className=" flex justify-center items-center">
-        <form action="https://getform.io/f/fb692458-f269-42f2-83a9-4a521c02e758" method="POST">
-    <input type="text" name="name"/>
-    <input type="email" name="email"/>
-    <input type="text" name="message"/>
-    
-    <input type="hidden" name="_gotcha" style={{  display:"none" }} />
-    
-    <input type="checkbox" name="subscribe" value="yes" checked/>
-    <input type="hidden" name="subscribe" value="no"/>
-    
-    <input type="radio" name="gender" value="male" checked/>
-    <input type="radio" name="gender" value="female"/>
-    <input type="radio" name="gender" value="other"/>
-    
-    <select name="work-experience">
-        <option value="one-year">0-1 years</option>
-        <option value="one-five-years">1-5 years</option>
-    </select>
-    <button type="submit">Send</button>
-</form>
+        <form action="https://getform.io/f/fb692458-f269-42f2-83a9-4a521c02e758" method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input  
+              type="text"
+              name="name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+              Let's talk
+            </button>
+          </form>
         </div>
       </div>
     </div>
